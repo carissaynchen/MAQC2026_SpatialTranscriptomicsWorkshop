828092ec7f50cbaa6bc5bf8da6ef7fb94e5b5b49
Carissa Yan Nan Chen <72971065+carissaynchen@users.noreply.github.com>
2026-09-16 18:27:37 +0200

    add dependencies

--- install_packages.R
+++ install_packages.R
@@ -1,6 +1,7 @@
 BiocManager::install(
-  c(
-    "SpatialExperiment"
+  c("DESpace", "ggplot2", "grid", "dplyr", "tidyr", "OSTA.data", "patchwork", "pheatmap", "ggspavis", "scuttle",
+    "SpatialExperiment", "scRNAseq", "scrapper", "sf", "SingleR", "SpaceTrooper", "spatialFDA",
+    "SpatialExperimentIO", "SpatialFeatureExperiment", "spdep", "Voyager"
   ),
   ask = FALSE,
   update = FALSE
