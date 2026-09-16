829f4c1007e7213eb5dd11a02c099599d115e8e8
Carissa Yan Nan Chen <72971065+carissaynchen@users.noreply.github.com>
2026-09-17 01:28:56 +0200

    Add Ncpus parameter to BiocManager install

--- install_packages.R
+++ install_packages.R
@@ -1,8 +1,8 @@
 BiocManager::install(
-  c("DESpace", "ggplot2", "grid", "dplyr", "tidyr", "OSTA.data", "patchwork", "pheatmap", "ggspavis", "scuttle",
+  c("DESpace", "ggplot2", "dplyr", "tidyr", "OSTA.data", "patchwork", "pheatmap", "ggspavis", "scuttle",
     "SpatialExperiment", "scRNAseq", "scrapper", "sf", "SingleR", "SpaceTrooper", "spatialFDA",
     "SpatialExperimentIO", "SpatialFeatureExperiment", "spdep", "Voyager"
   ),
   ask = FALSE,
-  update = FALSE
+  update = FALSE, Ncpus=4
 )
