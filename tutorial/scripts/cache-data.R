eb92e60c9ebad3e0c0b5874282fe85c6fa723cca
Carissa Yan Nan Chen <72971065+carissaynchen@users.noreply.github.com>
2026-09-17 11:09:12 +0200

    remove dir.create

--- tutorial/scripts/cache-data.R
+++ tutorial/scripts/cache-data.R
@@ -1,9 +1,3 @@
-dir.create(
-  "/home/rstudio/.cache/R/BiocFileCache",
-  recursive = TRUE,
-  showWarnings = FALSE
-)
-
 id <- "CosMx1k_MouseBrain2"
 path <- OSTA.data::OSTA.data_load(id, mol = FALSE)
 
@@ -12,3 +6,6 @@ message("Cached dataset: ", path)
 
 id <- "Xenium_HumanBreast1_Janesick"
 pa <- OSTA.data_load(id, mol=FALSE)
+
+stopifnot(file.exists(path))
+message("Cached dataset: ", path)
